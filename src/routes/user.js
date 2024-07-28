9263fbe3e0e3bbd78d5a8ecde364cebd8aba6856
Rahul Kumar <rahulkumar290399@gmail.com>
2024-07-28 12:40:29 +0530

    Lecture-49(ReactJs)-JWT

---
+++ src/routes/user.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { getHome, postLogin, postSignup } from "../controller/user.js";
+
+const router = express.Router();
+
+router.post('/signup', postSignup);
+
+// router.post('/login', postLogin)
+router.route('/login').post(postLogin);
+
+// home route
+router.get('/', getHome);
+
+router.get('/logout', (req, res, next) => {
+    res.
+    status(200)
+    .cookie("jwt","",{
+        maxAge: 0
+    })
+    .json({
+        message: "Logged out successfully"
+    })
+
+})
+
+
+export default router;
